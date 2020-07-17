7e41b41ec0e9584bc6201aa33a2770e935e5be75
alvaroef <a.espinosaf.2019@alumnos.urjc.es>
2020-07-17 23:12:59 +0200

    P7 Ex1

--- P7/Ex01-ping.py
+++ P7/Ex01-ping.py
@@ -29,8 +29,6 @@ print(f"Response received!: {r1.status} {r1.reason}\n")
 # -- Read the response's body
 data1 = r1.read().decode()
 
-# -- Create a variable with the data,
-# -- form the JSON received
 response = json.loads(data1)
 
 ping = response['ping']
