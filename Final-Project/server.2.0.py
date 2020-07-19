6d7aced36020aa70af31f91e5d32ede2f76a3be5
alvaroef <a.espinosaf.2019@alumnos.urjc.es>
2020-07-19 21:08:20 +0200

    do_GET + index

--- Final-Project/server.2.0.py
+++ Final-Project/server.2.0.py
@@ -19,3 +19,29 @@ socketserver.TCPServer.allow_reuse_address = True
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
 # It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')  # green request line
+
+        req_line = self.requestline.split(' ')  # splits the request line (by the spaces)
+
+        arguments = (req_line[1]).split("?")
+
+        first_argument = arguments[0]  # sets the first argument
+
+        contents = Path('Error.html').read_text()  # no argument --> error form
+        self.send_response(404)
+
+        # Main
+        try:
+
+            # Index
+            if first_argument == "/":  # return an HTML page with the forms for accessing to all the previous services
+
+                contents = Path('index.html').read_text()  # contents displayed in index.html
+                self.send_response(200)
